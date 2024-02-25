bb79f15418ff2f0ce44ee03d2c2d67cfd3fc00da
Teghjyot Singh <teghjyot@icloud.com>
2024-02-26 09:36:06 +1030

    Initial Commit

---
+++ oop-s1-2023/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  oop-s1-2023
+//
+//  Created by Devjyot Singh on 26/2/2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
