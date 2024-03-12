f6a784b416296aae250ca8a8774afc32cd7970a3
Teghjyot Singh <teghjyot@icloud.com>
2024-03-13 05:23:05 +1030

    this is stupid

--- main.cpp
+++ main.cpp
@@ -8,6 +8,44 @@
 #include <iostream>
 #include "workshop.h"
 
+double arrayMax(double *db_arr, int n)
+{
+	double	max;
+	
+	max = *db_arr;
+	while (n--)
+	{
+		if (*db_arr > max)
+			max = *db_arr;
+		db_arr++;
+	}
+	return (max);
+}
+
+double	*dynamicArray(int n, double M)
+{
+	double	*ptr;
+	
+	ptr = new double[n];
+	while (n--)
+		ptr[n] = M;
+	return (ptr);
+}
+
+void	printArray(double *db_arr, int n)
+{
+	while (n--)
+	{
+		std::cout << *db_arr << std::endl;
+		db_arr++;
+	}
+}
+
+void	changeValue(double *db)
+{
+	*db = 42;
+}
+
 void	pointer_fun(double	*debe, char	*str, double db, char c)
 {
 	debe = &db;
